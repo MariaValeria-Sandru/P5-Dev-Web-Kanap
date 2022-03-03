ba0423bed8393272db822d404a148f1081ca6917
MariaValeria-Sandru <valy895@yahoo.com>
2022-03-03 09:49:19 +0100

    Co-authored-by: brainafesoh <brainafesoh@users.noreply.github.com>

--- front/js/index.js
+++ front/js/index.js
@@ -24,12 +24,18 @@ function displayData(data) {
         </article>
         </a>
       `;
-     
+
     console.log(product);
   });
 }
 
-recupData().then((data) => displayData(data));
+recupData()
+  .then((data) => displayData(data))
+  .catch((error) => {
+    alert(
+      "Desolé mais la liste des produits est indisponible pour l'instant. \n Veuillez ressayer plus tard."
+    );
+  });
 
 /* (async () => {
   const data = await recupData();
